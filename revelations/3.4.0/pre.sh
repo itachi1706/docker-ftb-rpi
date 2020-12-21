88f2a98662fb0307f8502516c88721b325b58161
Kenneth Soh <kennethsohyq@gmail.com>
2020-12-22 01:25:44 +0800

    Added custom ftbstart script

--- revelations/3.4.0/pre.sh
+++ revelations/3.4.0/pre.sh
@@ -3,3 +3,7 @@
 chmod +x linux
 mkdir server
 ./linux 35 174 --path server
+cd server
+cp forge-*-universal.jar ftb.jar
+cp ../ftbstart.sh .
+cd ..
--- revelations/latest/pre.sh
+++ revelations/latest/pre.sh
@@ -3,3 +3,7 @@
 chmod +x linux
 mkdir server
 ./linux 35 --latest --path server
+cd server
+cp forge-*-universal.jar ftb.jar
+cp ../ftbstart.sh .
+cd ..
