4d9b64ef695ec6c907a3d20885aedad547409ad3
Kenneth Soh <kennethsohyq@gmail.com>
2020-12-23 00:43:55 +0800

    Wrong forgejar

--- revelation/3.3.0/settings.sh
+++ revelation/3.3.0/settings.sh
@@ -14,7 +14,7 @@ export MCVER="1.12.2"
 export JARFILE="minecraft_server.${MCVER}.jar"
 export LAUNCHWRAPPERVERSION="1.12"
 export LAUNCHWRAPPER="net/minecraft/launchwrapper/${LAUNCHWRAPPERVERSION}/launchwrapper-${LAUNCHWRAPPERVERSION}.jar"
-export FORGEJAR="FTBserver-1.12.2-14.23.5.2846-universal.jar"
+export FORGEJAR="forge-1.12.2-14.23.5.2846-universal.jar"
 
 ###################################################
 # Default arguments for JVM
